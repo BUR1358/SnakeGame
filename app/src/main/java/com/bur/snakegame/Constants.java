f52171d7e05e15679bfc3c2221a73972a7831f38
Alexandr Blinov <bur1358@yandex.ru>
2022-06-01 13:43:25 +0300

    Initial commit

---
+++ app/src/main/java/com/bur/snakegame/Constants.java
@@ -0,0 +1,6 @@
+package com.bur.snakegame;
+
+public class Constants {
+    public static int SCREEN_WIDTH;
+    public static int SCREEN_HEIGHT;
+}
---
+++ app/src/main/java/com/bur/snakegame/PartSnake.java
@@ -0,0 +1,82 @@
+package com.bur.snakegame;
+
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+
+public class PartSnake {
+    private Bitmap bm;
+    private int x, y;
+    private static int scr_h = 2220;
+    private static int scr_w = 1080;
+    private Rect rBody, rTop, rBottom, rLeft, rRight;
+
+    public PartSnake(Bitmap bm, int x, int y) {
+        this.bm = bm;
+        this.x = x;
+        this.y = y;
+    }
+
+    public Bitmap getBm() {
+        return bm;
+    }
+
+    public void setBm(Bitmap bm) {
+        this.bm = bm;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Rect getrBody() {
+        return new Rect(this.x, this.y, this.x + GameView.sizeOfMap, this.y + GameView.sizeOfMap);
+    }
+
+    public void setrBody(Rect rBody) {
+        this.rBody = rBody;
+    }
+
+    public Rect getrTop() {
+        return new Rect(this.x, this.y - 10 * Constants.SCREEN_HEIGHT / scr_h, this.x + GameView.sizeOfMap, this.y);
+    }
+
+    public void setrTop(Rect rTop) {
+        this.rTop = rTop;
+    }
+
+    public Rect getrBottom() {
+        return new Rect(this.x, this.y + GameView.sizeOfMap, this.x + GameView.sizeOfMap, this.y + GameView.sizeOfMap + 10 * Constants.SCREEN_HEIGHT / scr_h);
+    }
+
+    public void setrBottom(Rect rBottom) {
+        this.rBottom = rBottom;
+    }
+
+    public Rect getrLeft() {
+        return new Rect(this.x - 10 * Constants.SCREEN_WIDTH / scr_w, this.y, this.x, this.y + GameView.sizeOfMap);
+    }
+
+    public void setrLeft(Rect rLeft) {
+        this.rLeft = rLeft;
+    }
+
+    public Rect getrRight() {
+        return new Rect(this.x + GameView.sizeOfMap, this.y, this.x + GameView.sizeOfMap + 10 * Constants.SCREEN_WIDTH / scr_w, this.y + GameView.sizeOfMap);
+    }
+
+    public void setrRight(Rect rRight) {
+        this.rRight = rRight;
+    }
+}
